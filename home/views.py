3acc65cbc2cef95d78912da95164b0708a41034e
Dhrushit Raval <dr9703@rit.edu>
2022-03-05 20:23:10 -0500

    adding maps file

--- home/views.py
+++ home/views.py
@@ -5,3 +5,6 @@ from neva.settings import GMAP_LINK
 
 def home(request):
     return render(request, 'home.html', {'GMAP_LINK': GMAP_LINK})
+
+def maps(request):
+    return render(request, 'map.html', {'GMAP_LINK': GMAP_LINK})
