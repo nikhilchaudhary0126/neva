c798e59646e6a210afdeecbb2ac190da54107f56
vama-rit <vs4608@rit.edu>
2022-03-05 21:21:49 -0500

    adding login and registration page post request handling

--- home/views.py
+++ home/views.py
@@ -5,12 +5,52 @@ from home.forms import LocationForm
 from home.models import Location
 from neva.settings import GMAP_LINK, API_KEY, MAP_URL, GOOGLE_MAPS_API_KEY
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+
+def applogin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/home/')
+        else:
+            messages.success(request, "Incorrect details! Please try again.")
+            return render(request, 'login.html', {'form': AuthenticationForm()})
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def registration(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        name = request.POST['name']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if username and name and email and password1 and password2:
+            if password1 == password2:
+                user = User.objects.create_user(username=username, password=password1, email=email)
+                user.first_name = name
+                user.save()
+                messages.success(request, "User created. Please login.")
+                return redirect('/login')
+            else:
+                messages.error(request, "Passwords don't match")
+                return render(request, 'registration.html', {"form": UserCreationForm()})
+        else:
+            messages.error(request, "Registration failed! Please try again.")
+    return render(request, 'registration.html', {"form": UserCreationForm()})
 
 def home(request):
     f1 = Location.objects.all()
     print(f1)
-    data = Location.objects.get(id=1)
-    print(data.latitude,data.longitude)
+    # data = Location.objects.get(id=1)
+    # print(data.latitude,data.longitude)
 
     f2 = Location.objects.all().filter(addresstype="Shelter")
     print(f2)
